f09fdd158fb626f8205f9b144b2cf9ae64fb4193
ThisaraK943 <IT22221346@my.sliit.lk>
2025-10-23 16:20:02 +0530

    upto  main all done

--- src/main.go
+++ src/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 )
+
+type Server interface {
+	Addr() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, req *http.Request)
+}
 
 type simpleserver struct {
 	addr  string
@@ -19,8 +26,22 @@ func newSimpleServer(addr string) *simpleserver {
 		addr:  addr,
 		proxy: *httputil.NewSingleHostReverseProxy(serverUrl),
 	}
-
 }
+
+type LoadBalancer struct {
+	port              string
+	roundRobinCounter int
+	servers           []Server
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:              port,
+		roundRobinCounter: 0,
+		servers:           servers,
+	}
+}
+
 func handleErr(err error) {
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
@@ -28,3 +49,11 @@ func handleErr(err error) {
 	}
 }
 
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	// implementation will be added later
+	return nil
+}
+
+func (lb *LoadBalancer) ServeProxy(rw http.ResponseWriter, r *http.Request) {
+	// implementation will be added later
+}
