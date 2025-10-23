84239f9e00d8090bb3b362d2c49670da28257d9f
ThisaraK943 <IT22221346@my.sliit.lk>
2025-10-23 16:02:02 +0530

    fix: correct package declaration and improve server structure

--- src/main.go
+++ src/main.go
@@ -1,2 +1,30 @@
-packege main 
+package main
+
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type simpleserver struct {
+	addr  string
+	proxy httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleserver {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleserver{
+		addr:  addr,
+		proxy: *httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+
+}
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
 
